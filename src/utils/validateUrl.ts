41c0b492e526ea34d9c614563f5001da81e51b74
Hugo Corona <hugo.corona.r@gmail.com>
2025-03-16 12:06:47 -0700

    Refactor validateUrl

--- src/utils/validateUrl.ts
+++ src/utils/validateUrl.ts
@@ -1,8 +1,7 @@
-const URL_REGEX =
-  /^(http(s)?:\/\/)[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$/;
+
 
 export const validateUrl = (url: string) => {
   const trimedUrl = url.trim();
 
-  return !!trimedUrl.match(URL_REGEX);
+  return URL.canParse(trimedUrl);
 }
