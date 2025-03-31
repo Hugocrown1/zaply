f07534164702a1feba5e065fdfbedbd11a2661bb
Hugo Corona <hugo.corona.r@gmail.com>
2025-03-30 22:20:35 -0700

    Enhance Home page layout with welcoming message and improved styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,16 @@ import UrlShortener from "@/components/UrlShortener";
 
 export default function Home() {
   return (
-    <>
-      <h1>Zaply App</h1>
-
-      <UrlShortener />
-    </>
+    <section className="w-full h-screen">
+      <div className="flex flex-col items-center justify-start space-y-16 w-full h-full max-w-[1250px] mx-auto">
+        <div className="text-center mt-36">
+          <h1 className="text-6xl font-bold">Welcome to Zaply</h1>
+          <p className="text-xl font-semibold">
+            Optimize your links, enhance your reach!
+          </p>
+        </div>
+        <UrlShortener />
+      </div>
+    </section>
   );
 }
