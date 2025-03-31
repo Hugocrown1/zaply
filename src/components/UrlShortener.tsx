9f3c36ccb50e1c9241bc58aba678a4962c3e0904
Hugo Corona <hugo.corona.r@gmail.com>
2025-03-30 22:20:52 -0700

    Add URL shortening functionality with success notification and local storage support

--- src/components/UrlShortener.tsx
+++ src/components/UrlShortener.tsx
@@ -4,14 +4,28 @@ import React, { useState } from "react";
 import UrlShortenerForm from "./UrlShortenerForm";
 import { ShortUrlData } from "@/interfaces/ShortUrlData";
 import ShortUrlItem from "./ShortUrlItem";
+import { toast } from "sonner";
 
 const UrlShortener = () => {
   const [shortUrlItem, setShortUrlItem] = useState<ShortUrlData>();
+
+  const handleNewShortUrl = (urlData: ShortUrlData) => {
+    setShortUrlItem(urlData);
+
+    const shortUrlList = JSON.parse(
+      localStorage.getItem("shortUrlList") || "[]"
+    );
+    const newShortUrlList = [urlData, ...shortUrlList];
+    localStorage.setItem("shortUrlList", JSON.stringify(newShortUrlList));
+
+    toast.success("URL shortened successfully!");
+  };
+
   return (
-    <section className="flex flex-col w-[400px]">
-      <UrlShortenerForm onNewShortUrl={setShortUrlItem} />
+    <div className="flex flex-col w-full max-w-[550px] mx-auto space-y-2">
+      <UrlShortenerForm onNewShortUrl={handleNewShortUrl} />
       {shortUrlItem && <ShortUrlItem {...shortUrlItem} />}
-    </section>
+    </div>
   );
 };
 
