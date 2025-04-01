bef64f44d6c049c766801875291689001f2866c9
Hugo Corona <hugo.corona.r@gmail.com>
2025-03-31 22:40:11 -0700

    Add beta label to Navbar for version indication

--- src/components/ui/Navbar.tsx
+++ src/components/ui/Navbar.tsx
@@ -5,7 +5,12 @@ const Navbar = () => {
   return (
     <nav className="w-full h-[50px] bg-background-950 border-b-[1px] border-[#444444] ">
       <div className="flex items-center justify-between h-full px-4 w-full max-w-[1400px] mx-auto">
-        <Link href={"/"}>⚡Zaply</Link>
+        <div className="flex flex-row items-center space-x-2">
+          <Link href={"/"}>⚡Zaply</Link>
+          <div className="px-2 py-1 border-[1px] border-background-900 rounded-md text-xs">
+            beta
+          </div>
+        </div>
         <UrlsSheet />
       </div>
     </nav>
